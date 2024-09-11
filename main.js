3e9b060bf62e30cc7878496c775b1831a8115f6e
Rana Almalki <ranaalmalki12@gmail.com>
2024-09-11 14:57:54 +0300

    not solve??

---
+++ main.js
@@ -0,0 +1,66 @@
+
+
+
+
+
+let bodyd = document.body
+let divMove  = document.querySelector("div")
+// var newRandomColor = Math.floor(Math.random()*16777215).toString(16)
+
+
+    divMove.style.backgroundColor = newRandomc
+
+    let newRandomn = (m)=>{
+        return ath.floor(math.round()*m)
+    }
+
+    let newRandomc = ()=>{
+const h = newRandomn(360) ;
+const s = newRandomn(100) ;
+const i = newRandomn(100) ;
+return `hsl(${h}deg, ${s}, ${i}%)`;
+
+    }
+
+
+// function getcolor(){
+//     return Math.floor(Math.random()*16777215).toString(16)
+
+// }
+
+
+// divMove.style.backgroundColor= math.floor(math.round()*255)
+// bodyd.divMove.style.left ="100px"
+// divMove.style.left ="100px"
+
+// move.style.backgroundColor="black"
+// move.style.border="black"
+// move.style.gridColumn="2"
+
+ 
+
+
+
+// let third =document.querySelector("div")
+
+// third.style.backgroundColor="pink"
+// third.style.border="pink"
+// third.style.gridColumn="3"
+// third.style.gridAutoRows="3"
+
+
+
+
+// math.round(move)
+
+
+// let fourth =move.style.backgroundColor="green"
+
+
+
+
+
+
+// let fifth =move.style.backgroundColor="brown"
+
+// move.innerHTML=Math.random(second)
